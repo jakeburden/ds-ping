74c36f29601866d10827388ebf2c426cb244370d
Adam Chambers <adammchambers@gmail.com>
2016-04-04 20:55:42 -0400

    Abstracted JS from index.html into js/index and using API yet to be written... so cool

---
+++ browser/js/index.js
@@ -0,0 +1,55 @@
+// var example = [
+//    {
+// 	   url: 'digitalsurgeons.com',
+// 	   status: 200
+//    },
+//    {
+// 	   url: 'nugofiber.digitalsurgeons.com',
+// 	   status: 200
+//    },
+//    {
+// 	   url: 'skillsoft.com',
+// 	   status: 200
+//    }
+// ];
+
+function checkStatus() {
+	console.log(this.responseText);
+	var result = JSON.parse(this.responseText);
+
+	result.every(function(res) {
+	   if (res.status !== 200) {
+		   soundTheAlarm();
+		   return false;
+	   }
+
+	   return true;
+	});
+}
+
+function soundTheAlarm() {
+	console.log('Alarm!!');
+
+	var audioContext = new AudioContext();
+
+	var osc = audioContext.createOscillator();
+	osc.type = 'sawtooth';
+	osc.frequency.value = 440;
+	osc.connect(audioContext.destination);
+	osc.start(0);
+
+	var gain = audioContext.createGain();
+	gain.gain.value = 100;
+	gain.connect(osc.frequency);
+
+	var osc2 = audioContext.createOscillator();
+	osc2.type = 'sawtooth';
+	osc2.frequency.value = 1;
+	osc2.connect(gain);
+	osc2.start(0);
+}
+
+var xhr = new XMLHttpRequest();
+xhr.addEventListener('load', checkStatus);
+xhr.open('GET', 'http://localhost:9090/logs/latest');
+xhr.send();
